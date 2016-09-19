e75470e1db17f4dbb82a8018589edc66f9347781
Shirokami <shirokami649@gmail.com>
2016-09-18 21:59:38 -0500

    Construyendo la base, Objeto de Proceso y Recurso; La creacion de algunos recursos que podran utilizarse posteriormente

--- src/curly/memory/CurlyMemory.java
+++ src/curly/memory/CurlyMemory.java
@@ -6,12 +6,32 @@
 package curly.memory;
 
 public class CurlyMemory {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    //Lista de recursos disponibles
+    static Recurso recursos[];
+    //Lista de procesos listos
+    private static Proceso procesos_listos[];
+    //Lista de procesos bloqueados
+    private static Proceso procesos_bloqueados[];
+    //Lista de procesos suspendidos
+    private static Proceso procesos_suspendidos[];
+    //Lista de procesos suspendidos listos
+    private static Proceso procesos_suspendidos_listos[];
+    //Lista de procesos suspendidos bloqueados
+    private static Proceso procesos_suspendidos_bloqueados[];
+    
+    
+    //Metodo principal
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        //Inicializamos la lista de recursos
+        recursos = new Recurso[4];
+        //Creamos los recursos que estaran disponibles para los procesos
+        recursos[0] = new Recurso(1,"Impresora");
+        recursos[1] = new Recurso(2,"Raton");
+        recursos[2] = new Recurso(3,"Teclado");
+        recursos[3] = new Recurso(4,"CD-ROM");
+    }    
     
 }
